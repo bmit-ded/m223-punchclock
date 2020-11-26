8bf48288da37f8f85c13d78cbbcd3a3fe306d05e
dimitri <dimitris.derungs@espas.ch>
2020-11-26 05:18:23 -0800

    implemented sign up

---
+++ src/main/resources/public/registry.js
@@ -0,0 +1,27 @@
+const URL = 'http://localhost:8081';
+
+document.addEventListener('DOMContentLoaded', function(){
+    const createLoginForm = document.querySelector('#regForm');
+    createLoginForm.addEventListener('submit', signup);
+});
+
+const signup = (e) =>{
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const signup = {};
+    signup['username'] = formData.get('username');
+    signup['password'] = formData.get('password');
+
+    fetch(`${URL}/users/sign-up`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(signup)
+    }).then((result) => {
+        console.log(result);
+        if(result.status === 200){
+            window.location.replace(`${URL}/index.html`);
+        }
+    });
+}
