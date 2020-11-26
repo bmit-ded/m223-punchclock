0f4232604b68eddd9fa551a5a3cc87ec1a226723
dimitri <dimitris.derungs@espas.ch>
2020-11-26 07:29:43 -0800

    final commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO APPLICATION_USER(username, password) VALUES ('user', '$2a$10$ceov5tprx7.UpP7m6XCdneiPjeSYwb7tYuBw/3OdoEpboqUu0LvdC');
+
+INSERT INTO ENTRY (check_in, check_out, applicationuser_id) VALUES ('2020-11-26T12:12:12.345Z', '2020-11-27T12:12:12.345Z', 1);
